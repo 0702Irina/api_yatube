9e95be3a74b96eb9731a77cda38c1f288a195d91
Ирина Сичкарева <0702irisha@mail.ru>
2023-04-10 11:56:08 +0200

    final1

---
+++ README.md
@@ -0,0 +1,21 @@
+# API_YATUBE
+
+API для социальной сети YAtube
+
+## Технологии
+
+PYTHON 3.10
+Django 3.2
+Django Rest Framework 3.12.4
+
+### Запуск проекта в Dev-режиме
+
+    Установите и активируйте виртуальное окружение
+
+    Установите зависимости из файла requirements.txt (pip install -r requirements.txt)
+
+    В дирректории проекта выполните команду: python3 homework.py
+
+#### Автор
+
+Сичкарева Ирина
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,21 +1,23 @@
 from rest_framework import routers
-
+from rest_framework.authtoken import views
 from django.urls import include, path
-
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
-from rest_framework.authtoken import views
-
-
 router_v1 = routers.DefaultRouter()
-router_v1.register('posts', PostViewSet, basename='posts')
-router_v1.register(
-    'posts/(?P<post_id>\\d+)/comments', CommentViewSet, basename='comments'
-)
-router_v1.register('groups', GroupViewSet, basename='groups')
+router_v1.register('posts',
+                   PostViewSet,
+                   basename='posts')
+router_v1.register('posts/(?P<post_id>\\d+)/comments',
+                   CommentViewSet,
+                   basename='comments')
+router_v1.register('groups',
+                   GroupViewSet,
+                   basename='groups')
 
 
 urlpatterns = [
-    path('v1/api-token-auth/', views.obtain_auth_token),
-    path('v1/', include(router_v1.urls)),
+    path('v1/api-token-auth/',
+         views.obtain_auth_token),
+    path('v1/',
+         include(router_v1.urls)),
 ]
